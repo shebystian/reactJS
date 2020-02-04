8fe0e6852aa5fd42c3af3ae5ee7e6f339e7e637a
Sebastián Sánchez <sebastian.sanchez@viajesfalabella.com>
2020-02-04 18:12:24 -0300

    Add files via upload
    primera subida

---
+++ src/shoppingList.js
@@ -0,0 +1,14 @@
+class ShoppingList extends React.Component {
+    render() {
+      return (
+        <div className="shopping-list">
+          <h1>Lista de compras para {this.props.name}</h1>
+          <ul>
+            <li>Instagram</li>
+            <li>WhatsApp</li>
+            <li>Oculus</li>
+          </ul>
+        </div>
+      );
+    }
+  }
