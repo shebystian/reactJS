6603b877742c13316109d78efcd73a1aab37adfa
Sebastián Sánchez <sebastian.sanchez@viajesfalabella.com>
2020-02-07 00:18:52 -0300

    se agregan estilos css
    Signed-off-by: Sebastián Sánchez <sebastian.sanchez@viajesfalabella.com>

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import cardImg from '../images/kjhgfd_2048x.jpg'
+import './styles/Cards.css'
 
 class Card extends React.Component{
     render(){
         return (
-            <div>
-                <div>
-                    <img src ={cardImg} />
+            <div className ="card mx-auto Fitness-Card">
+                <div className="card-body">
+                    <div className="row center">
+                        <div className ="col-6">
+                            <img className="float-right" src ={cardImg} style={{height:'70%', width: '70%'}}/>
+                        </div>
+                        <div>
+                            <h1> Cards </h1>
+                            <p>travels cards</p>
+                        </div>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <h1> Cards </h1>
-                    <p>travels cards</p>
-                </div>
-            </div>
             )
     }
 }
+//con esto hago que sea exportable esta clase
 export default Card
