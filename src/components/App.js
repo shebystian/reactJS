39901de2313b957db1e8bcb3fdbd0871f59fd417
Sebastián Sánchez <sebastian.sanchez@viajesfalabella.com>
2020-04-29 01:12:59 -0400

    [fix] video 18 (compila pero no muestra nada) problema con react-router-dom creo...

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import ExercisesContainer from '../pages/Exercises'
 import ExerciseNewContainer from '../pages/ExcerciseNew'
+import NotFound from '../pages//404'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
             <Switch>
                 <Route exact path="/exercise" component={ExercisesContainer} />
                 <Route exact path="/exercise/new" component={ExerciseNewContainer} />
+                <Route component={NotFound} />
             </Switch>
         </BrowserRouter>
     )
